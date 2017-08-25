5877823c9a32ce27f114bb716bc4ae620ad2fd36
Brent <puddletowndesign@gmail.com>
2017-08-25 16:53:51 -0500

    updated git preferences

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 rm ~/.gitconfig
 rm ~/.gitignore_global
-ln -s ~/Documents/Dev.gitconfig ~/.gitconfig
-ln -s ~/Documents/Dev.gitignore_global ~/.gitignore_global
+ln -s ~/Documents/Dev/.gitconfig ~/.gitconfig
+ln -s ~/Documents/Dev/.gitignore_global ~/.gitignore_global
